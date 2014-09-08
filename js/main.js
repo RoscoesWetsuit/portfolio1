2a3520cf17f8fb94b310e2ce9fb763a8cab2b7d8
Cade Guevara <cadeguevara@gmail.com>
2014-09-08 09:41:10 -0700

    i finished my project. added parallax (i think).

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-main.js
+$(document).ready(function(){	
+	$('#nav').localScroll(800);		
+	$('#intro').parallax("50%", 0.1);	
+	$('#second').parallax("50%", 0.1);	
+	$('.bg').parallax("50%", 0.4);	
+	$('#third').parallax("50%", 0.3);
+})
